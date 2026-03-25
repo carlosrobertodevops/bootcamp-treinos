c898831edf3fc166fe5a0ba61cc83c226652cf9d
“carlosrobertodevops” <carlos.roberto.devops@gmail.com>
2026-03-25 10:20:24 -0300

    chore: add adjust of prettier and eslinte

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,40 @@
-import js from "@eslint/js";
-import { defineConfig } from "eslint/config";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import importPlugin from 'eslint-plugin-import'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**', 'src/generated/**'],
+  },
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node },
   },
-  tseslint.configs.recommended,
-  eslintConfigPrettier,
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: {
+      globals: globals.node,
+    },
     plugins: {
-      "simple-import-sort": simpleImportSort,
+      import: importPlugin,
+      'simple-import-sort': simpleImportSort,
     },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
+      semi: ['error', 'never'],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'import/first': 'error',
+      'import/newline-after-import': 'error',
+      'import/no-duplicates': 'error',
     },
   },
+  eslintConfigPrettier,
 ]);
